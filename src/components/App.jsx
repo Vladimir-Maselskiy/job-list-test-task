cf94d770b54e01012f72e050575ba3220e9b9420
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-11-05 17:16:58 +0200

    feat: conect tailwind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,6 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
       React homework template
     </div>
   );
